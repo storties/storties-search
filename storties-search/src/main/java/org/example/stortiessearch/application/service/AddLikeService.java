5db2133b7be95a8ca0c9da30b4dca9e4b34b8a4c
김경민 <kkm07133@naver.com>
2025-07-03 11:46:38 +0900

    feat: (#6) 좋아요 생성 서비스 추가

---
+++ storties-search/src/main/java/org/example/stortiessearch/application/service/AddLikeService.java
@@ -0,0 +1,22 @@
+package org.example.stortiessearch.application.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.stortiessearch.common.AuthenticatedUserProvider;
+import org.example.stortiessearch.infrastructure.grpc.user.AuthenticatedUser;
+import org.example.stortiessearch.persistence.CommandPostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AddLikeService {
+
+    private final CommandPostRepository commandPostRepository;
+
+    private final AuthenticatedUserProvider authenticatedUserProvider;
+
+    public void execute(Long postId) {
+        AuthenticatedUser user = authenticatedUserProvider.getAuthenticatedUser();
+
+        commandPostRepository.savePostLike(postId, user.userId());
+    }
+}
