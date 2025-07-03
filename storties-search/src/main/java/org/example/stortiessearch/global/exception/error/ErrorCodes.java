0ea9e0b691041b0c48b6bbf122c34327c51e7a23
김경민 <kkm07133@naver.com>
2025-07-03 11:33:07 +0900

    feat: (#6) 예외 처리 방식 개선

--- storties-search/src/main/java/org/example/stortiessearch/global/exception/error/ErrorCodes.java
+++ storties-search/src/main/java/org/example/stortiessearch/global/exception/error/ErrorCodes.java
@@ -3,6 +3,7 @@ package org.example.stortiessearch.global.exception.error;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.example.stortiessearch.global.exception.StortiesException;
+import org.springframework.boot.web.error.Error;
 
 @Getter
 @AllArgsConstructor
@@ -10,6 +11,8 @@ public enum ErrorCodes {
 
     PASSWORD_MISMATCH(ErrorStatus.UNAUTHORIZED, "비밀번호가 일치되지 않습니다.", 1),
     TOKEN_EXPIRED(ErrorStatus.UNAUTHORIZED, "토큰이 만료되었습니다.", 2),
+
+    POST_DELETE_FORBIDDEN(ErrorStatus.FORBIDDEN, "삭제 권한이 없습니다.", 1),
 
     USER_NOT_FOUND(ErrorStatus.NOT_FOUND, "유저를 찾지 못했습니다.", 1),
     POST_NOT_FOUND(ErrorStatus.NOT_FOUND, "게시물을 찾지 못했습니다.", 2),
