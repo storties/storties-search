1395a149fea2fafe4467ee93eef9805239b019fa
김경민 <kkm07133@naver.com>
2025-07-03 11:39:54 +0900

    feat: (#6) Post 삭제서비스 추가

---
+++ storties-search/src/main/java/org/example/stortiessearch/application/service/DeletePostService.java
@@ -0,0 +1,34 @@
+package org.example.stortiessearch.application.service;
+
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.example.stortiessearch.common.AuthenticatedUserProvider;
+import org.example.stortiessearch.global.exception.error.ErrorCodes;
+import org.example.stortiessearch.infrastructure.grpc.user.AuthenticatedUser;
+import org.example.stortiessearch.persistence.CommandPostRepository;
+import org.example.stortiessearch.persistence.QueryPostRepository;
+import org.example.stortiessearch.persistence.model.PostEntity;
+import org.example.stortiessearch.persistence.repository.PostJpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeletePostService {
+
+    private final CommandPostRepository commandPostRepository;
+
+    private final QueryPostRepository queryPostRepository;
+
+    private final AuthenticatedUserProvider authenticatedUserProvider;
+
+    public void execute(Long postId) {
+        PostEntity postEntity = queryPostRepository.queryPostById(postId);
+        AuthenticatedUser authenticatedUser = authenticatedUserProvider.getAuthenticatedUser();
+
+        if(!Objects.equals(authenticatedUser.userId(), postEntity.getId())) {
+           throw ErrorCodes.POST_DELETE_FORBIDDEN.throwException();
+        }
+
+        commandPostRepository.deletePostById(postId);
+    }
+}
