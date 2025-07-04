d30848e01b23d70a10d44f2180529f15448f309f
김경민 <kkm07133@naver.com>
2025-07-04 20:00:42 +0900

    feat: (#11) 벡터 변환을 위한 VectorRestClient 추가

---
+++ storties-search/src/main/java/org/example/stortiessearch/infrastructure/client/rest/dto/VectorResponse.java
@@ -0,0 +1,11 @@
+package org.example.stortiessearch.infrastructure.client.rest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class VectorResponse {
+
+    private float[] vector;
+}
