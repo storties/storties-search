7b1908541dd4ce7f8c5ab906d38afbe51544f789
김경민 <kkm07133@naver.com>
2025-07-04 19:59:17 +0900

    feat: (#11) KafkaConfig 추가

---
+++ storties-search/src/main/java/org/example/stortiessearch/infrastructure/mq/config/KafkaConfig.java
@@ -0,0 +1,24 @@
+package org.example.stortiessearch.infrastructure.mq.config;
+
+import org.example.stortiessearch.application.event.UpdatePostEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, UpdatePostEvent> kafkaListenerContainerFactoryWithManualAck(
+        ConsumerFactory<String, UpdatePostEvent> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, UpdatePostEvent> factory =
+            new ConcurrentKafkaListenerContainerFactory<>();
+
+        factory.setConsumerFactory(consumerFactory);
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+        return factory;
+    }
+}
