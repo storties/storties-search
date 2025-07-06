549b8337d564bad45b6740846f36ed0b355f857f
김경민 <kkm07133@naver.com>
2025-07-06 20:59:00 +0900

    chore: (#11) DeleteLIke -> Unlike 로 변경

--- storties-search/src/main/java/org/example/stortiessearch/application/service/UnLikeService.java
+++ storties-search/src/main/java/org/example/stortiessearch/application/service/UnLikeService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class DeleteLikeService {
+public class UnLikeService {
 
     private final AuthenticatedUserProvider authenticatedUserProvider;
 
