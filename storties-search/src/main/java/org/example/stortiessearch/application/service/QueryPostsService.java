7e47c62a334a8ce88736fa329e70d87bd6d6f724
김경민 <kkm07133@naver.com>
2025-07-06 21:00:56 +0900

    chore: (#11) readOnly=true 추가

--- storties-search/src/main/java/org/example/stortiessearch/application/service/QueryPostsService.java
+++ storties-search/src/main/java/org/example/stortiessearch/application/service/QueryPostsService.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.example.stortiessearch.application.service.dto.response.PostsResponse;
 import org.example.stortiessearch.data.persistence.QueryPostRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class QueryPostsService {
 
     private final QueryPostRepository queryPostRepository;
