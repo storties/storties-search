d9248c2111f38f88b9f017cf8352e3bb1d06537e
김경민 <kkm07133@naver.com>
2025-07-06 20:54:46 +0900

    feat: (#11) 도큐먼트 날짜 형식 명시

--- storties-search/src/main/java/org/example/stortiessearch/data/search/document/PostDocument.java
+++ storties-search/src/main/java/org/example/stortiessearch/data/search/document/PostDocument.java
@@ -1,10 +1,12 @@
 package org.example.stortiessearch.data.search.document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -16,6 +18,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class PostDocument {
 
     @Id
@@ -34,10 +37,20 @@ public class PostDocument {
     @Field(type = FieldType.Keyword)
     private List<String> tags;
 
-    @Field(type = FieldType.Date)
+    @Field(
+        type = FieldType.Date_Nanos,
+        format = {},
+        pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS||epoch_millis"
+    )
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     private LocalDateTime createdAt;
 
-    @Field(type = FieldType.Date)
+    @Field(
+        type = FieldType.Date_Nanos,
+        format = {},
+        pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS||epoch_millis"
+    )
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS")
     private LocalDateTime updatedAt;
 
     @Field(type = FieldType.Boolean)
