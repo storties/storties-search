008fe78c33f7b2748b573fe927b75ec8e9d25901
김경민 <kkm07133@naver.com>
2025-07-06 20:59:33 +0900

    chore: (#11) 필요 없는 userId 값 삭제

--- storties-search/src/main/java/org/example/stortiessearch/application/service/dto/request/UpdatePostRequest.java
+++ storties-search/src/main/java/org/example/stortiessearch/application/service/dto/request/UpdatePostRequest.java
@@ -3,7 +3,6 @@ package org.example.stortiessearch.application.service.dto.request;
 import java.util.List;
 
 public record UpdatePostRequest (
-    Long userId,
     String title,
     String content,
     List<String> tags,
