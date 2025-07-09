c1126cf97e72c8232ade64d8e8b0b3c30197c526
김경민 <kkm07133@naver.com>
2025-07-09 22:15:06 +0900

    feat: (#20) PostResponse 형식 변경

--- storties-search/src/main/java/org/example/stortiessearch/application/service/dto/response/PostsResponse.java
+++ storties-search/src/main/java/org/example/stortiessearch/application/service/dto/response/PostsResponse.java
@@ -1,19 +1,27 @@
 package org.example.stortiessearch.application.service.dto.response;
 
+import java.util.HashMap;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.example.stortiessearch.data.persistence.post.model.PostEntity;
+import org.example.stortiessearch.data.search.post.document.PostDocument;
 
 @Getter
 @AllArgsConstructor
 public class PostsResponse {
 
-    private final List<PostResponse> postResponses;
+    private final List<PostDetailResponse> postDetailResponse;
 
-    public static PostsResponse from(Iterable<PostEntity> posts) {
-        return new PostsResponse(((List<PostEntity>)posts).stream()
-            .map(PostResponse::from)
-            .toList());
+    public static PostsResponse of(List<PostDocument> posts, HashMap<Long, Long> postLikes,
+                                   HashMap<Long, Long> postViews) {
+
+        return new PostsResponse(posts.stream().map(post -> {
+                Long postId = post.getId();
+                long likeCount = postLikes.getOrDefault(postId, 0L);
+                long viewCount = postViews.getOrDefault(postId, 0L);
+                return PostDetailResponse.of(post, likeCount, viewCount);
+            })
+            .toList()
+        );
     }
 }
