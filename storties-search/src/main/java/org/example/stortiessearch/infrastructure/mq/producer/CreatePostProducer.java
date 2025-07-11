804a319f0e14fdafaaec21f3a6b322ffcdec1562
김경민 <kkm07133@naver.com>
2025-07-11 23:23:01 +0900

    feat: (#22) 게시물 생성 재시도 추가

--- storties-search/src/main/java/org/example/stortiessearch/infrastructure/mq/producer/CreatePostProducer.java
+++ storties-search/src/main/java/org/example/stortiessearch/infrastructure/mq/producer/CreatePostProducer.java
@@ -2,18 +2,29 @@ package org.example.stortiessearch.infrastructure.mq.producer;
 
 import lombok.RequiredArgsConstructor;
 import org.example.stortiessearch.application.event.CreatePostEvent;
-import org.example.stortiessearch.infrastructure.mq.KafkaProperties;
+import org.example.stortiessearch.infrastructure.mq.dto.KafkaEvent;
+import org.example.stortiessearch.infrastructure.mq.util.JsonSerializer;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+
+import static org.example.stortiessearch.infrastructure.mq.KafkaProperties.CREATE_TOPIC;
 
 @RequiredArgsConstructor
 @Component
 public class CreatePostProducer {
 
-    private final KafkaTemplate<String, CreatePostEvent> kafkaTemplate;
+    private final KafkaTemplate<String, KafkaEvent> kafkaTemplate;
 
+    private final JsonSerializer jsonSerializer;
 
     public void publish(CreatePostEvent event) {
-        kafkaTemplate.send(KafkaProperties.CREATE_TOPIC, String.valueOf(event.getId()), event);
+        KafkaEvent kafkaEvent = KafkaEvent.builder()
+            .topic(CREATE_TOPIC)
+            .eventClass(CreatePostEvent.class)
+            .payload(jsonSerializer.toJson(event))
+            .retryCount(0)
+            .build();
+
+        kafkaTemplate.send(CREATE_TOPIC, kafkaEvent);
     }
 }
